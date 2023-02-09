9a619539feffb2eb4c272e869d239d4980f8528f
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2023-02-09 23:02:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 import streamlit as st
 import os
 from download import download
+from time import sleep
 
-st.header('App')
-button_click2 = st.button('Download the video File') 
+st.header('App') 
 
-cmd_input = st.text_input('Enter video url')
-password = st.text_input('Enter password')
-key = st.text_input('Enter key')
-youtube_ffmpeg_command = f'ffmpeg -re  -stream_loop -1 -i video.mp4 -c copy output.mp4 -c:v libx264 -c:a aac -f flv -b:v 750k -g 48 -keyint_min 48 -sc_threshold 0 rtmp://a.rtmp.youtube.com/live2/{key}'
+url = 'https://file-to-link-09.herokuapp.com/WhitE_DeviL09/736685/foetoall-A-lofi-boy-sitting-on-a.mp4'
+youtube_ffmpeg_command = f'ffmpeg -re  -stream_loop -1 -i video.mp4 -c copy output.mp4 -c:v libx264 -c:a aac -f flv -b:v 750k -g 48 -keyint_min 48 -sc_threshold 0 rtmp://a.rtmp.youtube.com/live2/a2c2-ww7x-cv80-f6b1-fzb6'
 
-button_click3 = st.button('Download')
+status = download(url,'video.mp4')
+st.markdown(status)
 
-button_click = st.button('START')
+sleep(100)
 
+text = os.popen(youtube_ffmpeg_command).read()
+st.markdown(text)
 
-
-if button_click == True:
-    with st.container():
-      if password == "PREMrajpaul1@123":
-        text = os.popen(youtube_ffmpeg_command).read()
-        st.markdown(text)
-      else:
-        st.markdown("Failed")
-        
-if button_click3 == True:
-    with st.container():
-      if password == "PREMrajpaul1@123":
-        status = download(cmd_input,'video.mp4')
-        st.markdown(status)
-      else:
-        st.markdown("Failed")
+st.text("Working")
